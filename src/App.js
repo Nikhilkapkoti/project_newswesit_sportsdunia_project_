09855cf0dab1e270718cde143ee49f0d90e98d63
Nikhilkapkoti <nikhilkapkoti32@gmail.com>
2025-01-06 21:29:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Newsapp from './Components/Newsapp';
 import Dashboard from './Components/Dashboard';
+import Signup from './Components/Signup'; // Import the Signup component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Newsapp />} />
+        <Route path="/signup" element={<Signup />} /> {/* Add this line */}
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
